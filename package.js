24da9c3fe59958dfb036b1b3cd0858e47a7dc272
Andrew Peter Prifer <prifer.andras@gmail.com>
2016-08-17 14:49:28 +0200

    Use forked slick until #1059 is addressed

--- package.js
+++ package.js
@@ -1,29 +1,28 @@
 Package.describe({
-  summary: "the last carousel you'll ever need",
-  name:    "andrewpeterprifer:slick",
-  version: "1.6.0",
-  git:     "https://github.com/andrewpeterprifer/meteor-slick"
+  summary: 'the last carousel you\'ll ever need',
+  name: 'andrewpeterprifer:slick',
+  version: '1.6.0',
+  git: 'https://github.com/andrewpeterprifer/meteor-slick',
 });
 
-Package.onUse(function(api) {
-  
-  api.versionsFrom("METEOR@0.9.3.1");
-  
+Package.onUse(function (api) {
+  api.versionsFrom('METEOR@0.9.3.1');
+
   api.use('jquery', 'client');
-  
-  api.addFiles([
-    "vendor/slick/slick/ajax-loader.gif",
-    "vendor/slick/slick/fonts/slick.eot",
-    "vendor/slick/slick/fonts/slick.svg",
-    "vendor/slick/slick/fonts/slick.ttf",
-    "vendor/slick/slick/fonts/slick.woff"
-  ], "client", { isAsset: true });
-  
-  api.addFiles([
-    "vendor/slick/slick/slick.css",
-    "vendor/slick/slick/slick.js",
-    "lib/overrides.css"
-  ], "client");
-  
-});
 
+  api.use('less');
+
+  api.addAssets([
+    'vendor/slick/slick/ajax-loader.gif',
+    'vendor/slick/slick/fonts/slick.eot',
+    'vendor/slick/slick/fonts/slick.svg',
+    'vendor/slick/slick/fonts/slick.ttf',
+    'vendor/slick/slick/fonts/slick.woff',
+  ], 'client');
+
+  api.addFiles([
+    'vendor/slick/slick/slick.css',
+    'vendor/slick/slick/slick.js',
+    'lib/theme.less',
+  ], 'client');
+});
